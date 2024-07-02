262c0e56be7211196dca97b26cdae79613dfa74b
Paweł Worzała <pawelworzala86@gmail.com>
2024-07-02 15:04:08 +0200

    auto

--- hole.js
+++ hole.js
@@ -2,14 +2,22 @@ const fs = require('fs')
 
 let sourceStr = fs.readFileSync('./test.js').toString()
 
-var word = ''
-var isText = false
 
-var GLOBAL = {parent:null,code:[]}
+
+function createPart(){
+    return {parent:null,code:[]}
+}
+
+var GLOBAL = createPart()//{parent:null,code:[]}
 
 //var activeCode = GLOBAL
 
 function parseBlock(source,activeCode){
+    var word = ''
+    var isText = false
+    var isFunction = false
+    var funcName = ''
+    var lastWord = ''
 
     for(let index=0;index<source.length;index++){
 
@@ -18,15 +26,24 @@ function parseBlock(source,activeCode){
         if((char=='\'')||(char=='"')){
             isText=!!!isText
         }
-        if(((char==' ')||(char=='\n')||(char=='\r'))&&!isText){
+        if(char=='('){
+            isFunction=true
+            funcName=word
+        }
+        if(char==')'){
+            isFunction=false
+        }
+        if(((char==' ')||(char=='\n')||(char=='\r')
+            ||(char=='('))&&!isText){
             if(word.length>0){
                 console.log('word: ',word)
             }
+            lastWord = word
             word = ''
         }else{
             word += char
         }
-
+        
         function getNewLineIndex(idx){
             var rest=''
             for(let id=idx;id<source.length;id++){
@@ -45,6 +62,15 @@ function parseBlock(source,activeCode){
                 }
             }
         }
+        function getEndFunctionName(idx){
+            var rest=''
+            for(let id=idx;id<source.length;id++){
+                rest+=source[id]
+                if(source[id]==')'){
+                    return {skipped:id,rest}
+                }
+            }
+        }
 
         if(word=='var'){
             var {skipped,rest} = getNewLineIndex(index+1)
@@ -53,7 +79,13 @@ function parseBlock(source,activeCode){
             index = skipped
             var line = word+rest
             console.log(line)
-            activeCode.code.push(line)
+            var params=line.trim().split(' ')
+            var obj={
+                kind: 'var',
+                name: params[1],
+                value: params[3],
+            }
+            activeCode.code.push(obj)
         }
         if(word=='function'){
             var {skipped,rest} = getFunction(index+1)
@@ -62,10 +94,58 @@ function parseBlock(source,activeCode){
             index = skipped
             var code = word+rest
             console.log(code)
-            activeCode.code.push(code)
+            var name = code.split(' ')[1].split('(')[0]
+            var lines = code.split('\n')
+            lines.splice(0,1)
+            lines.splice(lines.length-1,1)
+            var kod = lines.join('\r\n')
+            var obj={
+                kind: 'function',
+                name: name,
+                code: [],
+            }
+            //var inline = createPart()
+            parseBlock(kod,obj)
+            activeCode.code.push(obj)
+        }
+        if(isFunction){
+            //console.log('FUNCTION')
+            var len=word.length
+            var {skipped,rest} = getEndFunctionName(index-len)
+            console.log(skipped)
+            console.log(rest)
+            index = skipped
+            var line = word+rest
+            console.log(line)
+            //var params=line.trim().split(' ')
+            line=line.substring(1,line.length-1)
+            var params=line.split(',')
+            var obj={
+                kind: 'call',
+                name: funcName,
+                params: params,
+            }
+            activeCode.code.push(obj)
+            isFunction=false
+        }
+        if(word=='='){
+            /*var {skipped,rest} = getNewLineIndex(index)
+            console.log(skipped)
+            console.log(rest)
+            index = skipped
+            var line = word+rest
+            console.log(line)
+            var params=line.trim().split(' ')*/
+            var obj={
+                kind: 'assign',
+                lastWord,
+            }
+            activeCode.code.push(obj)
         }
 
     }
 
 }
 parseBlock(sourceStr,GLOBAL)
+
+fs.writeFileSync('./bh.exp.json',JSON.stringify(GLOBAL,null,4))
